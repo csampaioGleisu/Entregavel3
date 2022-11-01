73139bca96de1a85b5718585abf5d792a083fe78
csampaioGleisu <113738769+csampaioGleisu@users.noreply.github.com>
2022-10-31 23:34:31 -0300

    so ajustes

--- app.js
+++ app.js
@@ -13,46 +13,40 @@ var port = 3001;
 app.listen(port, function () {
   console.log(`App de Exemplo escutando na porta http://localhost:${port}/`);
 });
-function somar(a, b) {
+function soma(a, b) {
   return a + b;
 }
-app.post("/soma", function (req, res) {
-  var body = req.body;
-  var resultado = somar(body.a, body.b);
-
-  res.send(`O resultado da soma de ${body.a} e ${body.b} é ${resultado}`);
+app.post("/soma", (req, res) => {
+  const { a, b } = req.body;
+  const resultado = soma(a, b);
+  res.send(`O resultado da soma de ${a} e ${b} é ${resultado}`);
 });
 
 function subtrair(a, b) {
   return a - b;
 }
 
-app.post("/sub", function (req, res) {
-  var body = req.body;
-  var resultado = subtrair(body.a, body.b);
-
-  res.send(`O resultado da subtração de ${body.a} e ${body.b} é ${resultado}`);
+app.post("/sub", (req, res) => {
+  const { a, b } = req.body;
+  const resultado = subtrair(a, b);
+  res.send(`O resultado da subtração de ${a} e ${b} é ${resultado}`);
 });
 
-function dividir(a, b) {
+function divisao(a, b) {
   return a / b;
 }
-app.post("/div", function (req, res) {
-  var body = req.body;
-  var resultado = dividir(body.a, body.b);
-
-  res.send(`O resultado da divisão de ${body.a} e ${body.b} é ${resultado}`);
+app.post("/divisao", (req, res) => {
+  const { a, b } = req.body;
+  const resultado = divisao(a, b);
+  res.send(`O resultado da divisão de ${a} e ${b} é ${resultado}`);
 });
 
 function multiplicar(a, b) {
   return a * b;
 }
 
-app.post("/multi", function (req, res) {
-  var body = req.body;
-  var resultado = multiplicar(body.a, body.b);
-
-  res.send(
-    `O resultado da multiplicação de ${body.a} e ${body.b} é ${resultado}`
-  );
+app.post("/mult", (req, res) => {
+  const { a, b } = req.body;
+  const resultado = multiplicar(a, b);
+  res.send(`O resultado da multiplicação de ${a} e ${b} é ${resultado}`);
 });
